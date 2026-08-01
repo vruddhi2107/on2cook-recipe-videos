4efa04d38cb59cdc794c3de07b9771db1a286ba0
vruddhi192 <vruddhi.inventindia@gmail.com>
2026-08-01 16:17:02 +0530

    Add new recipe videos and update recipe data with thumbnails for deep fry and gravy

--- recipes-data.js
+++ recipes-data.js
@@ -18,35 +18,59 @@
 
 const RECIPE_VIDEOS = [
   {
-    title: 'Cake',
-    function: 'bake',
-    thumbnail: 'assets/recipes/cake.png',
-    videoUrl: 'https://player.vimeo.com/video/1210082457',
-    // chef: 'Chef Rahul',
-    duration: '2:27'
-  },
-  {
-    title: 'Moong Dal Halwa',
-    function: 'induction',
-    thumbnail: 'assets/recipes/halwa.png',
-    videoUrl: 'https://player.vimeo.com/video/1210082459',
-    // chef: 'Chef Rahul',
-    duration: '1:47'
-  },
-  {
-    title: 'Chicken Biryani',
-    function: 'microwave',
-    thumbnail: 'assets/recipes/biryani.png',
-    videoUrl: 'https://player.vimeo.com/video/1210082458',
-    // chef: 'Chef Rahul',
-    duration: '2:52'
-  },
-  {
-    title: 'Momos',
-    function: 'steam',
-    thumbnail: 'assets/recipes/momos.png',
-    videoUrl: 'https://player.vimeo.com/video/1210082461',
-    // chef: 'Chef Rahul',
-    duration: '2:07'
-  },
+  title: 'Deep Fry Like a Pro',
+  function: 'Deep Fry',
+  thumbnail: 'assets/recipes/deep-fy.png',
+  videoUrl: 'https://player.vimeo.com/video/1212001865',
+  // chef: '',
+  duration: '3:07'
+},
+{
+  title: 'Perfect Cake Every Time',
+  function: 'bake',
+  thumbnail: 'assets/recipes/cake.png',
+  videoUrl: 'https://player.vimeo.com/video/1212002256',
+  // chef: '',
+  duration: '2:27'
+},
+{
+  title: 'Perfect Chicken Biryani Every Time',
+  function: 'microwave',
+  thumbnail: 'assets/recipes/biryani.png',
+  videoUrl: 'https://player.vimeo.com/video/1212002332',
+  // chef: '',
+  duration: '2:52'
+},
+{
+  title: 'Perfect Grilled Sandwich Every Time',
+  function: 'grill',
+  thumbnail: 'assets/recipes/sandwich.png',
+  videoUrl: 'https://player.vimeo.com/video/1212003033',
+  // chef: '',
+  duration: '2:53'
+},
+{
+  title: 'Perfect Momos Every Time',
+  function: 'steam',
+  thumbnail: 'assets/recipes/momos.png',
+  videoUrl: 'https://player.vimeo.com/video/1212003052',
+  // chef: '',
+  duration: '2:07'
+},
+{
+  title: 'Perfect Moong Dal Halwa Every Time',
+  function: 'induction',
+  thumbnail: 'assets/recipes/halwa.png',
+  videoUrl: 'https://player.vimeo.com/video/1212003081',
+  // chef: '',
+  duration: '1:47'
+},
+{
+  title: 'The Perfect Gravy Every Time',
+  function: 'induction',
+  thumbnail: 'assets/recipes/gravy.png',
+  videoUrl: 'https://player.vimeo.com/video/1212003270',
+  // chef: '',
+  duration: '3:39'
+},
 ];
